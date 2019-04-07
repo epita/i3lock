be18c5940844ce4a0ab5f2e45cf8fb239e1a7c1c
Sevan Murriguian <murrig_s@epita.fr>
2019-04-07 18:38:35 +0200

    upd : display modifier when auth fail

--- unlock_indicator.c
+++ unlock_indicator.c
@@ -363,6 +363,51 @@ void draw_image(xcb_pixmap_t bg_pixmap, uint32_t *resolution) {
         }
     }
 
+    /* Display current modifier */
+    if (auth_state == STATE_AUTH_WRONG && (modifier_string != NULL)) {
+        int h = 50;
+        int w = 300;
+        int h_scaled = ceil(scaling_factor * h);
+        int w_scaled = ceil(scaling_factor * w);
+        cairo_surface_t *output_modifier = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, w_scaled, h_scaled);
+        cairo_t *ctx_modifier = cairo_create(output_modifier);
+        cairo_scale(ctx_modifier, scaling_factor, scaling_factor);
+
+        /* Bakcground */
+        cairo_text_extents_t extents;
+        cairo_set_font_size(ctx_modifier, 14.0);
+
+        cairo_text_extents(ctx_modifier, modifier_string, &extents);
+        int x = w / 2 - ((extents.width / 2) + extents.x_bearing);
+        int y = h / 2 - ((extents.height / 2) + extents.y_bearing);
+
+        cairo_set_source_rgb(ctx_modifier, 125.0 / 255, 51.0 / 255, 0);
+        cairo_move_to(ctx_modifier, x, y);
+        cairo_show_text(ctx_modifier, modifier_string);
+        cairo_close_path(ctx_modifier);
+
+        if (xr_screens > 0) {
+            /* Composite the unlock indicator in the middle of each screen. */
+            for (int screen = 0; screen < xr_screens; screen++) {
+                int x = xr_resolutions[screen].x + (xr_resolutions[screen].width / 2) - (w_scaled / 2);
+                int y = xr_resolutions[screen].y + (xr_resolutions[screen].height / 2) + h_scaled + 50 ;
+                cairo_set_source_surface(xcb_ctx, output_modifier, x, y);
+                cairo_rectangle(xcb_ctx, x, y, w_scaled, h_scaled);
+                cairo_fill(xcb_ctx);
+            }
+        } else {
+            /* We have no information about the screen sizes/positions, so we just
+             * place the unlock indicator in the middle of the X root window and
+             * hope for the best. */
+            int x = (last_resolution[0] / 2) - (w_scaled / 2);
+            int y = (last_resolution[1] / 2) + h_scaled + 100;
+            cairo_set_source_surface(xcb_ctx, output_modifier, x, y);
+            cairo_rectangle(xcb_ctx, x, y, w_scaled, h_scaled);
+            cairo_fill(xcb_ctx);
+        }
+
+    }
+
 /* #ifdef LOGOUT_KEYBIND */
     if (locked_time >= AUTHORIZED_LOCK_TIME) {
         int h = 80;
@@ -371,10 +416,10 @@ void draw_image(xcb_pixmap_t bg_pixmap, uint32_t *resolution) {
         int w_scaled = ceil(scaling_factor * w);
         cairo_surface_t *output_indicator = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, w_scaled, h_scaled);
         cairo_t *ctx_indicator = cairo_create(output_indicator);
+        cairo_scale(ctx_indicator, scaling_factor, scaling_factor);
 
         /* Bakcground */
         cairo_set_source_rgb(ctx_indicator, 125.0 / 255, 51.0 / 255, 0);
-        cairo_set_line_width(ctx_indicator, 1);
         cairo_rectangle(ctx_indicator, 0, 0, w, h);
         cairo_stroke_preserve(ctx_indicator);
         cairo_fill(ctx_indicator);
